0645a0dce00bd93389fdd0caf9960c555e82ec30
Kristian <kr.mendoza.sh@gmail.com>
2023-02-20 21:52:14 -0500

    added blue hover effect over divs

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const gridNumber= parseInt(prompt("How many square would you like in a line?: "));
+let gridNumber= parseInt(prompt("How many square would you like in a line? (choose less than 100): "));
+while (gridNumber > 100){
+    gridNumber= parseInt(prompt("How many square would you like in a line? (choose less than 100): "));
+}
+
 const gridSize= 960/gridNumber;
 const root= document.querySelector(":root");
 root.style.setProperty('--gridNumber', gridNumber);
@@ -8,9 +12,12 @@ function createGridItems(count){
     for (let i=0; i< count**2; i++){
         const template =document.createElement("div");
         template.className="gridItem";
-        document.getElementById("gridContainer").appendChild(template)
-
+        template.id=i;
+        template.addEventListener("mouseenter", (e) => 
+        {template.style.backgroundColor="blue"});
+        document.getElementById("gridContainer").appendChild(template);
     }
+    
 }
 
 createGridItems(gridNumber)
